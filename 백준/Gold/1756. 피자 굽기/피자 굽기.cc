cccf3f7d86e6ed3c4e2808deab0d6e9dc8616aa0
HwangSe <38585901+hsj4436@users.noreply.github.com>
2023-12-20 23:22:47 +0900

    [Gold V] Title: 피자 굽기, Time: 260 ms, Memory: 8176 KB -BaekjoonHub

--- 백준/Gold/1756. 피자 굽기/피자 굽기.cc
+++ 백준/Gold/1756. 피자 굽기/피자 굽기.cc
@@ -3,52 +3,44 @@
 #include <algorithm>
 
 int D, N;
-std::vector<int> diameters;
-
-int binary_search(int target, int last_index) {
-    int left = 0, right = last_index;
-    while (left < right) {
-        int mid = (left + right) / 2;
-        if (diameters[mid] < target) {
-            right = mid;
-        } else {
-            left = mid + 1;
-        }
-    }
-    return left - 1;
-}
+std::vector<int> oven;
+std::vector<int> pizzas;
 
 int main() {
     std::cin >> D >> N;
 
     int diameter;
     std::cin >> diameter;
-    diameters.push_back(diameter);
+    oven.push_back(diameter);
 
     for (int i = 1; i < D; i++) {
         std::cin >> diameter;
-        diameters.push_back(std::min(diameter, diameters[i - 1]));
+        oven.push_back(std::min(diameter, oven[i - 1]));
     }
 
-    int last_depth = D;
     for (int i = 0; i < N; i++) {
         int pizza = 0;
         std::cin >> pizza;
-        if (last_depth == -1) {
-            continue;
+        pizzas.push_back(pizza);
+    }
+
+    int pizza_index = 0;
+    int answer = D;
+    for (int i = D - 1; i > -1; i--) {
+        if (oven[i] >= pizzas[pizza_index]) {
+            answer = i + 1;
+            pizza_index++;
         }
-        int index = binary_search(pizza, last_depth);
-        if (index < 0) {
-            last_depth = -1;
-        } else {
-            last_depth = index;
+
+        if (pizza_index == N) {
+            break;
         }
     }
 
-    if (last_depth == -1) {
-        std::cout << "0\n";
+    if (pizza_index == N) {
+        std::cout << answer << "\n";
     } else {
-        std::cout << last_depth + 1 << "\n";
+        std::cout << "0\n";
     }
 
     return 0;
