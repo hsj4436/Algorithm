b93cc13f299e2c692374f953d974cd627c0fd73e
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-01-03 22:59:31 +0900

    [Platinum V] Title: 히스토그램, Time: 44 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.cc
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -4,51 +4,24 @@
 #include <stack>
 
 int N;
-std::vector<std::tuple<int, int, int>> vec;
-std::stack<std::pair<int, int>> stk;
+int heights[100002];
+std::stack<int> stk;
 
 int main() {
     std::cin >> N;
-
-    int height = 0;
     for (int i = 1; i < N + 1; i++) {
-        std::cin >> height;
-        if (height == 0) {
-            while (!stk.empty()) {
-                vec.push_back(std::make_tuple(stk.top().first, stk.top().second, i));
-                stk.pop();
-            }
-            continue;
-        }
-        if (stk.empty()) {
-            stk.push({height, i});
-        } else {
-            int start = 0;
-            while (!stk.empty()) {
-                if (stk.top().first <= height) {
-                    break;
-                } else {
-                    vec.push_back(std::make_tuple(stk.top().first, stk.top().second, i));
-                    start = stk.top().second;
-                    stk.pop();
-                }
-            }
-            if (start == 0) {
-                stk.push({height, i});
-            } else {
-                stk.push({height, start});
-            }
-        }
+        std::cin >> heights[i];
     }
 
-    while (!stk.empty()) {
-        vec.push_back(std::make_tuple(stk.top().first, stk.top().second, N + 1));
-        stk.pop();
-    }
-
+    stk.push(0);
     int answer = 0;
-    for (auto t : vec) {
-        answer = std::max(answer, std::get<0>(t) * (std::get<2>(t) - std::get<1>(t)));
+    for (int i = 1; i < N + 2; i++) {
+        while (!stk.empty() && heights[stk.top()] > heights[i]) {
+            int check = stk.top();
+            stk.pop();
+            answer = std::max(answer, heights[check] * (i - stk.top() - 1));
+        }
+        stk.push(i);
     }
 
     std::cout << answer << "\n";
