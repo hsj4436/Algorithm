443990c48c44baf8f3091b31d26843b3a390d1c6
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-01-14 17:47:04 +0900

    [Gold I] Title: 전구를 켜라, Time: 80 ms, Memory: 23740 KB -BaekjoonHub

--- 백준/Gold/2423. 전구를 켜라/전구를 켜라.cc
+++ 백준/Gold/2423. 전구를 켜라/전구를 켜라.cc
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 
-int N, M;
-char board[502][502];
-int visited[502][502];
+int N, M, K = 501;
+std::vector<std::pair<int, int>> adj[300001];
+
+int dijkstra(int start, int end) {
+    int dist[300001];
+    std::fill(dist, dist + 300001, 987654321);
+    int visited[300001];
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+
+    dist[start] = 0;
+    pq.push({0, start});
+    while (!pq.empty()) {
+        int cur = pq.top().second;
+        pq.pop();
+        if (visited[cur]) {
+            continue;
+        }
+        visited[cur] = 1;
+        for (auto p : adj[cur]) {
+            int next = p.first;
+            int cost = p.second;
+            if (dist[next] > dist[cur] + cost) {
+                dist[next] = dist[cur] + cost;
+                pq.push({dist[next], next});
+            }
+        }
+    }
+
+    return dist[end];
+}
 
 int main() {
     std::cin >> N >> M;
@@ -12,196 +40,26 @@ int main() {
     for (int i = 0; i < N; i++) {
         std::cin >> row;
         for (int j = 0; j < M; j++) {
-            board[i][j] = row[j];
-        }
-    }
-
-    for (int i = 0; i < N + 1; i++) {
-        for (int j = 0; j < M + 1; j++) {
-            visited[i][j] = 987654321;
-        }
-    }
-
-    /**
-     *  {x, y}, {direction, changed}
-     *  direction
-     *  0 - from left top
-     *  1 - from left bottom
-     *  2 - from right bottom
-     *  3 - from right top
-     */
-    std::queue<std::pair<std::pair<int, int>, std::pair<int, int>>> q;
-    visited[0][0] = 0;
-    if (board[0][0] == '/') {
-        visited[1][1] = 1;
-        q.push({{1, 1}, {0, 1}});
-    } else {
-        visited[1][1] = 0;
-        q.push({{1, 1}, {0, 0}});
-    }
-
-    while (!q.empty()) {
-        int x = q.front().first.first;
-        int y = q.front().first.second;
-        int direction = q.front().second.first;
-        int changed = q.front().second.second;
-        q.pop();
-
-        if (direction == 0) {
-            int nx = x - 1;
-            int ny = y;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed < visited[nx][ny + 1]) {
-                    visited[nx][ny + 1] = changed;
-                    q.push({{nx, ny + 1}, {1, changed}});
-                }
-                if (board[nx][ny] == '\\' && changed + 1 < visited[nx][ny + 1]){
-                    visited[nx][ny + 1] = changed + 1;
-                    q.push({{nx, ny + 1}, {1, changed + 1}});
-                }
-            }
-            nx = x;
-            ny = y;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed + 1 < visited[nx + 1][ny + 1]) {
-                    visited[nx + 1][ny + 1] = changed + 1;
-                    q.push({{nx + 1, ny + 1}, {0, changed + 1}});
-                }
-                if (board[nx][ny] == '\\' && changed < visited[nx + 1][ny + 1]){
-                    visited[nx + 1][ny + 1] = changed;
-                    q.push({{nx + 1, ny + 1}, {0, changed}});
-                }
-            }
-            nx = x;
-            ny = y - 1;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed < visited[nx + 1][ny]) {
-                    visited[nx + 1][ny] = changed;
-                    q.push({{nx + 1, ny}, {3, changed}});
-                }
-                if (board[nx][ny] == '\\' && changed + 1 < visited[nx + 1][ny]){
-                    visited[nx + 1][ny] = changed + 1;
-                    q.push({{nx + 1, ny}, {3, changed + 1}});
-                }
-            }
-        } else if (direction == 1) {
-            int nx = x - 1;
-            int ny = y - 1;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed + 1 < visited[nx][ny]) {
-                    visited[nx][ny] = changed + 1;
-                    q.push({{nx, ny}, {2, changed + 1}});
-                }
-                if (board[nx][ny] == '\\' && changed < visited[nx][ny]){
-                    visited[nx][ny] = changed;
-                    q.push({{nx, ny}, {2, changed}});
-                }
-            }
-            nx = x - 1;
-            ny = y;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed < visited[nx][ny + 1]) {
-                    visited[nx][ny + 1] = changed;
-                    q.push({{nx, ny + 1}, {1, changed}});
-                }
-                if (board[nx][ny] == '\\' && changed + 1 < visited[nx][ny + 1]){
-                    visited[nx][ny + 1] = changed + 1;
-                    q.push({{nx, ny + 1}, {1, changed + 1}});
-                }
-            }
-            nx = x;
-            ny = y;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed + 1 < visited[nx + 1][ny + 1]) {
-                    visited[nx + 1][ny + 1] = changed + 1;
-                    q.push({{nx + 1, ny + 1}, {0, changed + 1}});
-                }
-                if (board[nx][ny] == '\\' && changed < visited[nx + 1][ny + 1]){
-                    visited[nx + 1][ny + 1] = changed;
-                    q.push({{nx + 1, ny + 1}, {0, changed}});
-                }
-            }
-        } else if (direction == 2) {
-            int nx = x;
-            int ny = y - 1;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed < visited[nx + 1][ny]) {
-                    visited[nx + 1][ny] = changed;
-                    q.push({{nx + 1, ny}, {3, changed}});
-                }
-                if (board[nx][ny] == '\\' && changed + 1 < visited[nx + 1][ny]){
-                    visited[nx + 1][ny] = changed + 1;
-                    q.push({{nx + 1, ny}, {3, changed + 1}});
-                }
-            }
-            nx = x - 1;
-            ny = y - 1;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed + 1 < visited[nx][ny]) {
-                    visited[nx][ny] = changed + 1;
-                    q.push({{nx, ny}, {2, changed + 1}});
-                }
-                if (board[nx][ny] == '\\' && changed < visited[nx][ny]){
-                    visited[nx][ny] = changed;
-                    q.push({{nx, ny}, {2, changed}});
-                }
-            }
-            nx = x - 1;
-            ny = y;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed < visited[nx][ny + 1]) {
-                    visited[nx][ny + 1] = changed;
-                    q.push({{nx, ny + 1}, {1, changed}});
-                }
-                if (board[nx][ny] == '\\' && changed + 1 < visited[nx][ny + 1]){
-                    visited[nx][ny + 1] = changed + 1;
-                    q.push({{nx, ny + 1}, {1, changed + 1}});
-                }
-            }
-        } else {
-            int nx = x - 1;
-            int ny = y - 1;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed + 1 < visited[nx][ny]) {
-                    visited[nx][ny] = changed + 1;
-                    q.push({{nx, ny}, {2, changed + 1}});
-                }
-                if (board[nx][ny] == '\\' && changed < visited[nx][ny]){
-                    visited[nx][ny] = changed;
-                    q.push({{nx, ny}, {2, changed}});
-                }
-            }
-            nx = x;
-            ny = y - 1;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed < visited[nx + 1][ny]) {
-                    visited[nx + 1][ny] = changed;
-                    q.push({{nx + 1, ny}, {3, changed}});
-                }
-                if (board[nx][ny] == '\\' && changed + 1 < visited[nx + 1][ny]){
-                    visited[nx + 1][ny] = changed + 1;
-                    q.push({{nx + 1, ny}, {3, changed + 1}});
-                }
-            }
-            nx = x;
-            ny = y;
-            if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-                if (board[nx][ny] == '/' && changed + 1 < visited[nx + 1][ny + 1]) {
-                    visited[nx + 1][ny + 1] = changed + 1;
-                    q.push({{nx + 1, ny + 1}, {0, changed + 1}});
-                }
-                if (board[nx][ny] == '\\' && changed < visited[nx + 1][ny + 1]){
-                    visited[nx + 1][ny + 1] = changed;
-                    q.push({{nx + 1, ny + 1}, {0, changed}});
-                }
+            if (row[j] == '/') {
+                adj[i * K + j].push_back({(i + 1) * K + (j + 1), 1});
+                adj[(i + 1) * K + (j + 1)].push_back({i * K + j, 1});
+                adj[(i + 1) * K + j].push_back({i * K + (j + 1), 0});
+                adj[i * K + (j + 1)].push_back({(i + 1) * K + j, 0});
+            } else {
+                adj[i * K + j].push_back({(i + 1) * K + (j + 1), 0});
+                adj[(i + 1) * K + (j + 1)].push_back({i * K + j, 0});
+                adj[(i + 1) * K + j].push_back({i * K + (j + 1), 1});
+                adj[i * K + (j + 1)].push_back({(i + 1) * K + j, 1});
             }
         }
     }
 
-    if (visited[N][M] == 987654321) {
+   int ans = dijkstra(0, N * K + M);
+
+    if (ans == 987654321) {
         std::cout << "NO SOLUTION\n";
     } else {
-        std::cout << visited[N][M] << "\n";
+        std::cout << ans << "\n";
     }
     return 0;
 }
