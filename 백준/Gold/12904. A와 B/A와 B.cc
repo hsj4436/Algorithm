e3ece7900074068ab218449cdd47398b4647e806
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-02-04 23:35:59 +0900

    [Gold V] Title: A와 B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.cc
+++ 백준/Gold/12904. A와 B/A와 B.cc
@@ -2,43 +2,26 @@
 #include <algorithm>
 
 std::string S, T;
-bool possible = false;
-
-void back_track(std::string str) {
-    if (str == S) {
-        possible = true;
-        return;
-    }
-
-    if (str.back() == 'A') {
-        std::string tmp = str;
-        tmp.pop_back();
-        back_track(tmp);
-        if (possible) {
-            return;
-        }
-    }
-
-    if (str.back() == 'B') {
-        std::string tmp = str;
-        tmp.pop_back();
-        std::reverse(tmp.begin(), tmp.end());
-        back_track(tmp);
-        if (possible) {
-            return;
-        }
-    }
-}
 
 int main() {
     std::cin >> S >> T;
 
-    back_track(T);
-
-    if (possible) {
-        std::cout << "1\n";
-    } else {
-        std::cout << "0\n";
+    while (true) {
+        if (S.size() == T.size()) {
+            if (S == T) {
+                std::cout << "1\n";
+            } else {
+                std::cout << "0\n";
+            }
+            return 0;
+        }
+        
+        if (T.back() == 'A') {
+            T.pop_back();
+        } else {
+            T.pop_back();
+            std::reverse(T.begin(), T.end());
+        }
     }
 
     return 0;
