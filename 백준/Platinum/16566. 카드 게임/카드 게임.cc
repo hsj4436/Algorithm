cea1654ca9b8545f04753c6ad33c3f8f0a53f219
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-02-10 23:32:41 +0900

    [Platinum V] Title: 카드 게임, Time: 732 ms, Memory: 33276 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.cc
+++ 백준/Platinum/16566. 카드 게임/카드 게임.cc
@@ -3,50 +3,43 @@
 #include <algorithm>
 
 int N, M, K;
-int parents[4000001];
 std::vector<int> cards;
+int parent[4000001];
 
-int find_card(int x) {
-    if (parents[x] == x) {
-        return parents[x];
+int find_parent(int x) {
+    if (parent[x] == x) {
+        return x;
     } else {
-        return parents[x] = find_card(parents[x]);
+        return parent[x] = find_parent(parent[x]);
     }
-}
-
-void union_card(int a, int b) {
-    // b = a + 1
-    a = find_card(a), b = find_card(b);
-    if (a == b) {
-        return;
-    }
-    parents[a] = b;
 }
 
 int main() {
     std::cin.tie(NULL);
     std::ios::sync_with_stdio(false);
-
+    
     std::cin >> N >> M >> K;
 
-    for (int i = 1; i < N + 1; i++) {
-        parents[i] = i;
-    }
+    cards.resize(M);
 
-    int card;
     for (int i = 0; i < M; i++) {
-        std::cin >> card;
-        cards.push_back(card);
-        parents[card] = card;
+        std::cin >> cards[i];
     }
 
     std::sort(cards.begin(), cards.end());
 
-    for (int i = 0; i < K; i++) {
-        std::cin >> card;
-        int d = find_card(std::upper_bound(cards.begin(), cards.end(), card) - cards.begin());
-        std::cout << cards[d] << "\n";
-        union_card(d, d + 1);
+    for (int i = 1; i < N + 1; i++) {
+        parent[i] = i;
     }
+
+    int n;
+    for (int i = 0; i < K; i++) {
+        std::cin >> n;
+        auto it = std::upper_bound(cards.begin(), cards.end(), n);
+        int p = find_parent(*it);
+        std::cout << p << "\n";
+        parent[p] = p + 1;
+    }
+
     return 0;
 }
