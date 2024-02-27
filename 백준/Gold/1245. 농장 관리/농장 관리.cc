d8874410275edf375453e19ef5c832737e75c165
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-02-27 20:42:35 +0900

    [Gold V] Title: 농장 관리, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.cc
+++ 백준/Gold/1245. 농장 관리/농장 관리.cc
@@ -9,14 +9,12 @@ int dy[8] = {0, -1, 0, 1, 1, -1, 1, -1};
 int N, M;
 int farm[101][71];
 bool visited[101][71];
+bool is_top = true;
 int answer = 0;
 
-bool is_top(int x, int y) {
-    bool visited_copy[101][71];
-    std::fill(&visited_copy[0][0], &visited_copy[100][71], false);
-    int height = farm[x][y];
+void bfs(int x, int y) {
     std::queue<PII> q;
-    visited_copy[x][y] = true;
+    visited[x][y] = true;
     q.push({x, y});
 
     while (!q.empty()) {
@@ -32,21 +30,18 @@ bool is_top(int x, int y) {
                 continue;
             }
 
-            if (visited_copy[nx][ny]) {
+            if (farm[x][y] < farm[nx][ny]) {
+                is_top = false;
+            }
+
+            if (visited[nx][ny] || farm[x][y] != farm[nx][ny]) {
                 continue;
             }
 
-            if (farm[nx][ny] > height) {
-                return false;
-            }
-            if (farm[nx][ny] == height) {
-                visited_copy[nx][ny] = true;
-                q.push({nx, ny});
-            }
+            visited[nx][ny] = true;
+            q.push({nx, ny});
         }
     }
-
-    return true;
 }
 
 int main() {
@@ -58,67 +53,14 @@ int main() {
         }
     }
 
-    std::queue<PII> q;
-    q.push({0, 0});
-    while (!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        if (visited[x][y]) {
-            continue;
-        }
-        visited[x][y] = true;
-
-        if (is_top(x, y)) {
-            answer++;
-            int height = farm[x][y];
-            std::queue<PII> top_q;
-            top_q.push({x, y});
-            visited[x][y] = false;
-            while (!top_q.empty()) {
-                int cur_x = top_q.front().first;
-                int cur_y = top_q.front().second;
-                top_q.pop();
-
-                if (visited[cur_x][cur_y]) {
-                    continue;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (!visited[i][j]) {
+                is_top = true;
+                bfs(i, j);
+                if (is_top) {
+                    answer++;
                 }
-                visited[cur_x][cur_y] = true;
-
-                for (int i = 0; i < 8; i++) {
-                    int nx = cur_x + dx[i];
-                    int ny = cur_y + dy[i];
-
-                    if (nx < 0 || nx > N - 1 || ny < 0 || ny > M - 1) {
-                        continue;
-                    }
-
-                    if (visited[nx][ny]) {
-                        continue;
-                    }
-
-                    if (farm[nx][ny] == height) {
-                        top_q.push({nx, ny});
-                    } else if (farm[nx][ny] < height) {
-                        q.push({nx, ny});
-                    }
-                }
-            }
-        } else {
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx < 0 || nx > N - 1 || ny < 0 || ny > M - 1) {
-                    continue;
-                }
-
-                if (visited[nx][ny]) {
-                    continue;
-                }
-
-                q.push({nx, ny});
             }
         }
     }
