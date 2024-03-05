e42dbffc753332f436844310fd89b6b8bf43230c
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-03-05 18:42:28 +0900

    [Gold V] Title: 경쟁적 전염, Time: 20 ms, Memory: 2576 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
@@ -1,59 +1,68 @@
 #include <iostream>
 #include <queue>
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, -1, 0, 1};
+#define PII std::pair<int, int>
 
+int dx[4] = {-1, 0, 1, 0};
+
+int dy[4] = {0, -1, 0, 1};
 int N, K;
+
 int S, X, Y;
-std::pair<int, int> lab[201][201];
+int lab[201][201];
+
+struct cmp {
+    bool operator() (std::pair<int, PII> a, std::pair<int, PII> b) {
+        if (a.first == b.first) {
+            return lab[a.second.first][a.second.second] > lab[b.second.first][b.second.second];
+        } else {
+            return a.first > b.first;
+        }
+    }
+};
 
 int main() {
     std::cin >> N >> K;
 
-    int cell;
-    std::priority_queue<std::pair<int, std::pair<int, int>>> pq;
+    std::priority_queue<std::pair<int, PII>, std::vector<std::pair<int, PII>>, cmp> pq;
     for (int i = 1; i < N + 1; i++) {
         for (int j = 1; j < N + 1; j++) {
-            std::cin >> cell;
-            if (cell != 0) {
-                lab[i][j] = {cell, 0};
+            std::cin >> lab[i][j];
+            if (lab[i][j] > 0) {
+                pq.push({1, {i, j}});
             }
         }
     }
 
     std::cin >> S >> X >> Y;
 
-    for (int i = 0; i < S; i++) {
-        for (int j = 1; j < N + 1; j++) {
-            for (int k = 1; k < N + 1; k++) {
-                if (lab[j][k].second == i && lab[j][k].first != 0) {
-                    pq.push({-lab[j][k].first, {j, k}});
-                }
-            }
+    while (!pq.empty()) {
+        int t = pq.top().first;
+        int x = pq.top().second.first;
+        int y = pq.top().second.second;
+        pq.pop();
+
+        if (t == S + 1) {
+            break;
         }
-        while (!pq.empty()) {
-            int v = -pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-            pq.pop();
 
-            for (int j = 0; j < 4; j++) {
-                int nx = x + dx[j];
-                int ny = y + dy[j];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-                if (nx < 1 || nx > N || ny < 1 || ny > N) {
-                    continue;
-                }
-                if (lab[nx][ny].first != 0) {
-                    continue;
-                }
-                lab[nx][ny] = {v, i + 1};
+            if (nx < 1 || nx > N || ny < 1 || ny > N) {
+                continue;
             }
+            if (lab[nx][ny] != 0) {
+                continue;
+            }
+            lab[nx][ny] = lab[x][y];
+            pq.push({t + 1, {nx, ny}});
         }
     }
 
-    std::cout << lab[X][Y].first << "\n";
+
+    std::cout << lab[X][Y] << "\n";
 
     return 0;
 }
