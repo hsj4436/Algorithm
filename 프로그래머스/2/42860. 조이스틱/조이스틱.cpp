01edf7b7197b17ed685c361e27b991e47948dd7e
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-03-21 20:43:55 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -3,96 +3,51 @@
 
 using namespace std;
 
-string target;
-
-int backTrack(int index, string current) {
-    if (current == target) {
-        return 0;
-    }
-    int minMove = 100001;
-    
-    // moveRight
-    int rightIndex = index;
-    int moveCount = 0;
-    while (true) {
-        if (current[rightIndex] == target[rightIndex]) {
-            rightIndex++;
-            moveCount++;
-            rightIndex %= target.size();
-            continue;
-        }
-        char alphabet = current[rightIndex];
-        int up = 0;
-        while (alphabet != target[rightIndex]) {
-            alphabet++;
-            up++;
-            if (alphabet > 'Z') {
-                alphabet = 'A';
-            }
-        }
-        int down = 0;
-        alphabet = current[rightIndex];
-        while (alphabet != target[rightIndex]) {
-            alphabet--;
-            down++;
-            if (alphabet < 'A') {
-                alphabet = 'Z';
-            }
-        }
-        string nextName = current;
-        nextName[rightIndex] = target[rightIndex];
-        int temporalMove = moveCount + min(up, down) + backTrack(rightIndex, nextName);
-        minMove = min(minMove, temporalMove);
-        break;
-    }
-    
-    // moveLeft
-    int leftIndex = index - 1;
-    if (leftIndex < 0) {
-        leftIndex = target.size() - 1;
-    }
-    moveCount = 1;
-    while (true) {
-        if (current[leftIndex] == target[leftIndex]) {
-            leftIndex--;
-            moveCount++;
-            leftIndex += target.size();
-            leftIndex %= target.size();
-            continue;
-        }
-        char alphabet = current[leftIndex];
-        int up = 0;
-        while (alphabet != target[leftIndex]) {
-            alphabet++;
-            up++;
-            if (alphabet > 'Z') {
-                alphabet = 'A';
-            }
-        }
-        int down = 0;
-        alphabet = current[leftIndex];
-        while (alphabet != target[leftIndex]) {
-            alphabet--;
-            down++;
-            if (alphabet < 'A') {
-                alphabet = 'Z';
-            }
-        }
-        string nextName = current;
-        nextName[leftIndex] = target[leftIndex];
-        int temporalMove = moveCount + min(up, down) + backTrack(leftIndex, nextName);
-        minMove = min(minMove, temporalMove);
-        break;
-    }
-    
-    return minMove;
-}
-
 int solution(string name) {
     int answer = 0;
-    target = name;
     string current(name.size(), 'A');
-    answer = backTrack(0, current);
+    int index = 0;
+    while (current != name) {
+        int minMove = 1000001;
+        int minIndex = 0;
+        for (int i = 0; i < name.size(); i++) {
+            if (current[i] == name[i]) {
+                continue;
+            }
+            int minLeftRight = max(i, index) - min(i, index);
+            if (i > index) {
+                minLeftRight = min(minLeftRight, index + (int)name.size() - i);
+            } else {
+                minLeftRight = min(minLeftRight, (int)name.size() - index + i);
+            }
+            char alphabet = current[i];
+            int up = 0;
+            while (alphabet != name[i]) {
+                alphabet++;
+                up++;
+                if (alphabet > 'Z') {
+                    alphabet = 'A';
+                }
+            }
+            int down = 0;
+            alphabet = current[i];
+            while (alphabet != name[i]) {
+                alphabet--;
+                down++;
+                if (alphabet < 'A') {
+                    alphabet = 'Z';
+                }
+            }
+            if (min(up, down) + minLeftRight < minMove) {
+                minMove = min(up, down) + minLeftRight;
+                minIndex = i;
+            }
+        }
+        
+        answer += minMove;
+        index = minIndex;
+        current[index] = name[index];
+    }
     
     return answer;
 }
