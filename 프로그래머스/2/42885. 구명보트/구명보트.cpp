28c8ad6eaa4b52922fab3b17c850338caeea0928
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-03-21 21:47:48 +0900

    [level 2] Title: 구명보트, Time: 1.26 ms, Memory: 4.62 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(), people.end());
+    
+    int left = 0, right = people.size() - 1;
+    while (left <= right) {
+        if (left == right) {
+            answer++;
+            break;
+        }
+        if (people[left] + people[right] > limit) {
+            answer++;
+            right--;
+        } else {
+            answer++;
+            left++;
+            right--;
+        }
+    }
+    return answer;
+}
