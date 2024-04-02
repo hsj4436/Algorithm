c04daa1097c6fbc9defd7b7dc882bf2a2667059b
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-04-02 21:19:39 +0900

    [Gold III] Title: DFS 스페셜 저지, Time: 56 ms, Memory: 10164 KB -BaekjoonHub

--- 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.cc
+++ 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.cc
@@ -6,38 +6,17 @@ int N;
 bool visited[100001];
 std::vector<int> graph[100001];
 std::vector<int> inputOrder;
-int orderIndex = 0;
-bool isEnd = false;
+std::vector<int> generatedOrder;
+int nodeOrder[100001];
 
 void dfs(int cur) {
-    bool isLeaf = true;
     for (int next : graph[cur]) {
-        if (!visited[next]) {
-            isLeaf = false;
-            break;
+        if (visited[next]) {
+            continue;
         }
-    }
-    if (isLeaf) {
-        return;
-    }
-    auto next = std::find(graph[cur].begin(), graph[cur].end(), inputOrder[orderIndex]);
-    if (next == graph[cur].end()) {
-        isEnd = true;
-        return;
-    }
-    while (next != graph[cur].end()) {
-        int index = next - graph[cur].begin();
-        if (visited[graph[cur][index]]) {
-            isEnd = true;
-            return;
-        }
-        visited[graph[cur][index]] = true;
-        orderIndex++;
-        dfs(graph[cur][index]);
-        if (isEnd) {
-            return;
-        }
-        next = std::find(graph[cur].begin(), graph[cur].end(), inputOrder[orderIndex]);
+        visited[next] = true;
+        generatedOrder.push_back(next);
+        dfs(next);
     }
 }
 
@@ -55,28 +34,31 @@ int main() {
         graph[B].push_back(A);
     }
 
-    for (int i = 1; i < N + 1; i++) {
+    for (int i = 0; i < N; i++) {
         int v;
         std::cin >> v;
         inputOrder.push_back(v);
-        std::sort(graph[i].begin(), graph[i].end());
+        nodeOrder[v] = i;
     }
 
-    if (inputOrder[0] != 1) {
-        std::cout << "0\n";
-        return 0;
+    for (int i = 1; i < N + 1; i++) {
+        std::sort(graph[i].begin(), graph[i].end(), [&](int a, int b) {
+            return nodeOrder[a] < nodeOrder[b];
+        });
     }
 
     visited[1] = true;
-    orderIndex++;
+    generatedOrder.push_back(1);
     dfs(1);
 
-    if (orderIndex == N) {
-        std::cout << "1\n";
-    } else {
-        std::cout << "0\n";
+    for (int i = 0; i < N; i++) {
+        if (generatedOrder[i] != inputOrder[i]) {
+            std::cout << "0\n";
+            return 0;
+        }
     }
 
+    std::cout << "1\n";
 
     return 0;
 }
