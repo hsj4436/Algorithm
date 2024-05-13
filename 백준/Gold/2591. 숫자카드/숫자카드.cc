af9f915c9ead8f2ff89f0c9f18e295abe262c8cc
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-05-13 16:19:26 +0900

    [Gold V] Title: 숫자카드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2591. 숫자카드/숫자카드.cc
+++ 백준/Gold/2591. 숫자카드/숫자카드.cc
@@ -4,29 +4,35 @@
 
 std::string input;
 std::string numbers[35];
-LL dp[35][40];
+LL dp[41];
 
-LL sol(int nIndex, int inputIndex) {
-    if (inputIndex >= input.size()) {
+LL sol(int index) {
+    if (index == input.size()) {
         return 1;
     }
 
-    if (dp[nIndex][inputIndex] != -1) {
-        return dp[nIndex][inputIndex];
+    if (dp[index] != -1) {
+        return dp[index];
     }
 
-    dp[nIndex][inputIndex] = 0;
-    for (int i = 1; i < 35; i++) {
-        std::string token = input.substr(inputIndex, numbers[i].size());
-        if (token.size() != numbers[i].size()) {
-            continue;
-        }
-        if (token == numbers[i]) {
-            dp[nIndex][inputIndex] += sol(i, inputIndex + numbers[i].size());
-        }
+    dp[index] = 0;
+
+    if (input[index] == '0') {
+        return 0;
     }
 
-    return dp[nIndex][inputIndex];
+    if (index < input.size() - 1) {
+        std::string token = input.substr(index, 2);
+        if (std::stoi(token) < 35) {
+            dp[index] += sol(index + 2);
+            if (token[1] == '0') {
+                return dp[index];
+            }
+        }
+    }
+    dp[index] += sol(index + 1);
+
+    return dp[index];
 }
 
 int main() {
@@ -36,18 +42,9 @@ int main() {
         numbers[i] = std::to_string(i);
     }
 
-    std::fill_n(&dp[0][0], 35 * 40, -1);
+    std::fill_n(&dp[0], 41, -1);
 
-    LL answer = 0;
-    for (int i = 1; i < 35; i++) {
-        std::string token = input.substr(0, numbers[i].size());
-        if (token.size() != numbers[i].size()) {
-            continue;
-        }
-        if (token == numbers[i]) {
-            answer += sol(i, numbers[i].size());
-        }
-    }
+    LL answer = sol(0);
 
     std::cout << answer << "\n";
 
