a4e2b6a111606ae7f549028b8610730db220de4b
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-05-24 15:38:21 +0900

    [Gold V] Title: 두 수의 합, Time: 408 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/9024. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+
+int T;
+int N, K;
+int arr[1000001];
+
+int main() {
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+
+    std::cin >> T;
+
+    while (T--) {
+        std::cin >> N >> K;
+
+        for (int i = 0; i < N; i++) {
+            std::cin >> arr[i];
+        }
+
+        std::sort(arr, arr + N);
+
+        int answer = 0;
+        int maxDiff = 2000000001;
+        int left = 0, right = N - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (std::abs(sum - K) < maxDiff) {
+                maxDiff = std::abs(sum - K);
+                answer = 1;
+                if (sum - K < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            } else if (std::abs(sum - K) == maxDiff) {
+                answer++;
+                if (sum - K < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            } else {
+                if (sum - K < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        std::cout << answer << "\n";
+    }
+    return 0;
+}
