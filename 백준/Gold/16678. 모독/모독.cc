50df357cfc7d4fe7dec7ec9458eeff3e947698b0
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-06-26 14:16:59 +0900

    [Gold V] Title: 모독, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/16678. 모독/모독.cc
+++ 백준/Gold/16678. 모독/모독.cc
@@ -14,12 +14,15 @@ int main() {
     std::sort(honors + 1, honors + N + 1);
 
     long long hackers = 0;
-    for (int i = 1, base = 1; i < N + 1; i++) {
-        if (honors[i] < base) {
-            continue;
+    if (honors[1] != 1) {
+        hackers += honors[1] - 1;
+        honors[1] = 1;
+    }
+    for (int i = 2; i < N + 1; i++) {
+        if (honors[i] > honors[i - 1]) {
+            hackers += honors[i] - (honors[i - 1] + 1);
+            honors[i] = honors[i - 1] + 1;
         }
-        hackers += honors[i] - base;
-        base++;
     }
 
     std::cout << hackers << "\n";
