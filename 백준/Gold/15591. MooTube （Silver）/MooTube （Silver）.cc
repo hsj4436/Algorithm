655d3dcc87835690f1ac6161b1d9f50a5bf4d450
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-08-01 18:16:56 +0900

    [Gold V] Title: MooTube (Silver), Time: 436 ms, Memory: 2280 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.cc
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.cc
@@ -6,7 +6,6 @@
 
 int N, Queries;
 std::vector<PII> graph[5001];
-int USADO[5001];
 bool visited[5001];
 
 int main() {
@@ -22,38 +21,29 @@ int main() {
     int K, V;
     for (int i = 0; i < Queries; i++) {
         std::cin >> K >> V;
-        std::fill_n(&USADO[0], 5001, 1000000001);
         std::fill_n(&visited[0], 5001, false);
-        std::queue<PII> q;
+        std::queue<int> q;
         visited[V] = true;
-        q.push({V, 1000000001});
+        q.push(V);
 
+        int answer = 0;
         while (!q.empty()) {
-            int cur = q.front().first;
-            int u = q.front().second;
+            int cur = q.front();
             q.pop();
 
             for (PII next : graph[cur]) {
                 if (visited[next.first]) {
                     continue;
                 }
-                if (std::min(u, next.second) < USADO[next.first]) {
-                    visited[next.first] = true;
-                    USADO[next.first] = std::min(u, next.second);
-                    q.push({next.first, std::min(u, next.second)});
+                if (next.second < K) {
+                    continue;
                 }
+                answer++;
+                visited[next.first] = true;
+                q.push(next.first);
             }
         }
 
-        int answer = 0;
-        for (int j = 1; j < N + 1; j++) {
-            if (j == V) {
-                continue;
-            }
-            if (USADO[j] >= K) {
-                answer++;
-            }
-        }
         std::cout << answer << "\n";
     }
 
