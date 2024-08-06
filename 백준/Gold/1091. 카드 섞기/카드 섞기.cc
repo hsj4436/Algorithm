261b5c5fdb52a54eade94e29de882c29294862cc
HwangSe <38585901+hsj4436@users.noreply.github.com>
2024-08-06 15:07:35 +0900

    [Gold IV] Title: 카드 섞기, Time: 428 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+int N;
+int P[48];
+int S[48];
+std::vector<int> cards;
+
+bool isEnd() {
+    for (int i = 0; i < N; i++) {
+        if (P[cards[i]] != i % 3) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    std::cin >> N;
+
+    int p;
+    for (int i = 0; i < N; i++) {
+        std::cin >> p;
+        P[i] = p;
+    }
+
+    int s;
+    for (int i = 0; i < N; i++) {
+        std::cin >> s;
+        S[i] = s;
+    }
+
+    for (int i = 0; i < N; i++) {
+        cards.push_back(i);
+    }
+
+    int counter = 0;
+    while (!isEnd()) {
+        if (counter > 100000 * N) {
+            std::cout << "-1\n";
+            return 0;
+        }
+
+        std::vector<int> temp(N, 0);
+        for (int i = 0; i < N; i++) {
+            temp[S[i]] = cards[i];
+        }
+        cards = temp;
+        counter++;
+    }
+
+    std::cout << counter << "\n";
+
+    return 0;
+}
