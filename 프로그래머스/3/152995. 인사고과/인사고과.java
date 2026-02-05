eeaa0bd00eb137970dbb3169f3980df12485a27c
HwangSe <38585901+hsj4436@users.noreply.github.com>
2026-02-05 21:16:22 +0900

    [level 3] Title: 인사고과, Time: 138.00 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,35 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-        int answer = 0;
-        List<int[]> score = new ArrayList<>();
-        score.add(new int[]{scores[0][0], scores[0][1], 1});
-        for (int i = 1; i < scores.length; i++) {
-            score.add(new int[]{scores[i][0], scores[i][1], 0});
-        }
-        score.sort((s1, s2) -> {
-            return (s2[0] + s2[1]) - (s1[0] + s1[1]);
+        int answer = 1;
+        
+        int[] wanho = scores[0];
+        int maxCoworkerScore = 0;
+        
+        Arrays.sort(scores, (s1, s2) -> {
+            return s1[0] == s2[0] ? s1[1] - s2[1] : s2[0] - s1[0];
         });
         
-        int before = 200001, rank = 0;
-        for (int i = 0; i < score.size(); i++) {
-            if (score.get(i)[0] + score.get(i)[1] < before) {
-                boolean pass = true;
-                for (int j = 0; j < i; j++) {
-                    if (score.get(i)[0] < score.get(j)[0] && score.get(i)[1] < score.get(j)[1]) {
-                        pass = false;
-                        break;
-                    }
+        for(int[] score : scores) {
+            if (maxCoworkerScore <= score[1]) {
+                maxCoworkerScore = score[1];
+                if (score[0] + score[1] > wanho[0] + wanho[1]) {
+                    answer++;
                 }
-                if (pass) {
-                    rank++;
-                    if (score.get(i)[2] == 1) {
-                        return rank;
-                    }
-                } else {
-                    if (score.get(i)[2] == 1) {
-                        return -1;
-                    }
+            } else {
+                if (score.equals(wanho)) {
+                    return -1;
                 }
             }
         }
